d6a23d7a7f770a422f2a00ec696ffc0f62261319
Dik131 <d.i.k.93@mail.ru>
2025-08-25 15:37:06 +0300

    resolved conflicts

--- wallpost/src/features/PostLengthFilter/ui/PostLengthFilter.tsx
+++ wallpost/src/features/PostLengthFilter/ui/PostLengthFilter.tsx
@@ -5,10 +5,10 @@ type PostLengthFilterProps = {
 export const PostLengthFilter = ({ onChange }: PostLengthFilterProps) => {
   return (
     <select onChange={(e) => onChange(Number(e.target.value))}>
-      <option value="0">Все посты</option>
       <option value="5">5 постов</option>
       <option value="10">10 постов</option>
       <option value="20">20 постов</option>
+      <option value="0">Все посты</option>
     </select>
   );
 };
